8dc5b4e8f9647ea0833a79296baf5f52025807ac
Sanjula-lakshan <sanjularathnamalala2001@gmail.com>
2025-03-01 16:10:43 +0530

    Connect to the Chat Server

--- lib/screens/EyedraBot/chatbot.dart
+++ lib/screens/EyedraBot/chatbot.dart
@@ -23,5 +23,19 @@ class _ChatbotScreenState extends State<ChatbotScreen> {
     setState(() {
       messages.add({"role": "user", "content": message});
     });
+
+    try {
+      final response = await http.post(
+        Uri.parse('http://127.0.0.1:8000/chat'),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"message": message}),
+      );
+    } catch (e) {
+      setState(() {
+        messages.add({"role": "bot", "content": "Error: Unable to connect to server."});
+      });
+    }
   }
-}
+
+  }
+
