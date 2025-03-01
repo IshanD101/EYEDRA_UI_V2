b1f6146cb04b55562268f9c0301155d09200983a
Sanjula-lakshan <sanjularathnamalala2001@gmail.com>
2025-03-01 15:54:54 +0530

    Create the Main Chatbot Screen Class with Minimal Build Method

--- lib/screens/EyedraBot/chatbot.dart
+++ lib/screens/EyedraBot/chatbot.dart
@@ -2,3 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+
+class ChatbotScreen extends StatefulWidget {
+  const ChatbotScreen({super.key});
+
+  @override
+  _ChatbotScreenState createState() => _ChatbotScreenState();
+}
+
+class _ChatbotScreenState extends State<ChatbotScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // Temporary placeholder
+  }
+}
