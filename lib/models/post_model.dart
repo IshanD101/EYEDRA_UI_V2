ed1381f89d75c1b4ea9eacdd7480981eda035aa7
Dilethni <163492301+Dilethni@users.noreply.github.com>
2025-03-03 16:20:39 +0530

    code clean

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -1,19 +1,3 @@
 class Post {
-  final String username;
-  final String content;
-  final String imageUrl; // New field for images
-  int likes;
-  int dislikes;
-  int comments;
-  int shares;
-
-  Post({
-    required this.username,
-    required this.content,
-    required this.imageUrl, // Ensure this is provided
-    this.likes = 0,
-    this.dislikes = 0,
-    this.comments = 0,
-    this.shares = 0,
-  });
+  
 }
