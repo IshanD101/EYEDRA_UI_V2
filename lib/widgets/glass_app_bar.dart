be90fc55a38e5ea375cf467ea593c4f952bbbbc8
Chathnindu <chathnindu@gmail.com>
2025-03-09 18:26:38 +0530

    Making adjustments to glass_app_bar.dart file..

--- lib/widgets/glass_app_bar.dart
+++ lib/widgets/glass_app_bar.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
+import '../models/group.dart';
+import '../models/group_member.dart';
+import '../widgets/group_details_dialog.dart';
+import '../widgets/add_member_dialog.dart';
 
 class GlassAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final VoidCallback onBackPressed;
+  final Group group;
+  final Function(GroupMember) onMemberAdded;
 
   const GlassAppBar({
     Key? key,
     required this.title,
     required this.onBackPressed,
+    required this.group,
+    required this.onMemberAdded,
   }) : super(key: key);
 
   @override
@@ -17,12 +25,25 @@ class GlassAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text(
-        title,
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.blue[900],
+      title: GestureDetector(
+        onTap: () => _showGroupDetails(context),
+        child: Row(
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue[900],
+              ),
+            ),
+            const SizedBox(width: 4),
+            Icon(
+              Icons.info_outline,
+              size: 16,
+              color: Colors.white.withOpacity(0.7),
+            ),
+          ],
         ),
       ),
       backgroundColor: Colors.grey[900]?.withOpacity(0.7),
@@ -31,6 +52,26 @@ class GlassAppBar extends StatelessWidget implements PreferredSizeWidget {
         icon: const Icon(Icons.arrow_back, color: Colors.white),
         onPressed: onBackPressed,
       ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.person_add,
+            color: Colors.white.withOpacity(0.9),
+          ),
+          onPressed: () => _showAddMemberDialog(context),
+          tooltip: 'Add member',
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.more_vert,
+            color: Colors.white.withOpacity(0.9),
+          ),
+          onPressed: () {
+            // Show more options menu
+            _showMoreOptions(context);
+          },
+        ),
+      ],
       flexibleSpace: ClipRect(
         child: BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
@@ -39,6 +80,87 @@ class GlassAppBar extends StatelessWidget implements PreferredSizeWidget {
           ),
         ),
       ),
+    );
+  }
+
+  void _showGroupDetails(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => GroupDetailsDialog(group: group),
+    );
+  }
+
+  void _showAddMemberDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AddMemberDialog(
+        onMemberAdded: onMemberAdded,
+        existingMemberIds: group.members.map((m) => m.id).toList(),
+      ),
+    );
+  }
+
+  void _showMoreOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (context) => BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          decoration: BoxDecoration(
+            color: Colors.grey[900]?.withOpacity(0.9),
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+            border: Border.all(
+              color: Colors.white.withOpacity(0.1),
+              width: 1,
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildOptionTile(
+                context,
+                Icons.edit,
+                'Edit group info',
+                    () => Navigator.pop(context),
+              ),
+              _buildOptionTile(
+                context,
+                Icons.notifications,
+                'Mute notifications',
+                    () => Navigator.pop(context),
+              ),
+              _buildOptionTile(
+                context,
+                Icons.exit_to_app,
+                'Leave group',
+                    () => Navigator.pop(context),
+                isDestructive: true,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOptionTile(BuildContext context, IconData icon, String label, VoidCallback onTap, {bool isDestructive = false}) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        color: isDestructive ? Colors.red[300] : Colors.white.withOpacity(0.9),
+      ),
+      title: Text(
+        label,
+        style: TextStyle(
+          color: isDestructive ? Colors.red[300] : Colors.white.withOpacity(0.9),
+        ),
+      ),
+      onTap: onTap,
     );
   }
 }
