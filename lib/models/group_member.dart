d48d9d10ac8b3f8cf5592dec55662b9dbd8a1427
Chathnindu <chathnindu@gmail.com>
2025-03-10 14:14:52 +0530

    Making adjustments to group_member.dart file..

--- lib/models/group_member.dart
+++ lib/models/group_member.dart
@@ -1,13 +1,44 @@
 class GroupMember {
   final String id;
   final String name;
-  final String? avatarUrl;
   final bool isAdmin;
+  final bool isBanned;
+  final DateTime? bannedAt;
+  final String? bannedBy;
+  final String? banReason;
+  final String? avatarUrl;  // Added missing property
 
   GroupMember({
     required this.id,
     required this.name,
-    this.avatarUrl,
     this.isAdmin = false,
+    this.isBanned = false,
+    this.bannedAt,
+    this.bannedBy,
+    this.banReason,
+    this.avatarUrl,  // Added to constructor
   });
+
+  // Create a copy of this member with updated fields
+  GroupMember copyWith({
+    String? id,
+    String? name,
+    bool? isAdmin,
+    bool? isBanned,
+    DateTime? bannedAt,
+    String? bannedBy,
+    String? banReason,
+    String? avatarUrl,  // Added to copyWith method
+  }) {
+    return GroupMember(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      isAdmin: isAdmin ?? this.isAdmin,
+      isBanned: isBanned ?? this.isBanned,
+      bannedAt: bannedAt ?? this.bannedAt,
+      bannedBy: bannedBy ?? this.bannedBy,
+      banReason: banReason ?? this.banReason,
+      avatarUrl: avatarUrl ?? this.avatarUrl,  // Added to return statement
+    );
+  }
 }
