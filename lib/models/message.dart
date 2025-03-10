ab6ff91320ece27919dc7b65d850dd12fe7fb6aa
Chathnindu <chathnindu@gmail.com>
2025-03-10 14:13:14 +0530

    Making adjustments to message.dart file..

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,13 +1,50 @@
 class ChatMessage {
+  final String id;
   final String text;
   final bool isFromMe;
   final String? senderName;
+  final String senderId;
   final DateTime timestamp;
+  final bool isDeleted;
+  final String? deletedBy;
+  final DateTime? deletedAt;
 
   ChatMessage({
+    String? id,
     required this.text,
     required this.isFromMe,
     this.senderName,
+    required this.senderId,
     DateTime? timestamp,
-  }) : timestamp = timestamp ?? DateTime.now();
+    this.isDeleted = false,
+    this.deletedBy,
+    this.deletedAt,
+  }) :
+        this.id = id ?? DateTime.now().millisecondsSinceEpoch.toString(),
+        this.timestamp = timestamp ?? DateTime.now();
+
+  // Create a copy with updated properties
+  ChatMessage copyWith({
+    String? id,
+    String? text,
+    bool? isFromMe,
+    String? senderName,
+    String? senderId,
+    DateTime? timestamp,
+    bool? isDeleted,
+    String? deletedBy,
+    DateTime? deletedAt,
+  }) {
+    return ChatMessage(
+      id: id ?? this.id,
+      text: text ?? this.text,
+      isFromMe: isFromMe ?? this.isFromMe,
+      senderName: senderName ?? this.senderName,
+      senderId: senderId ?? this.senderId,
+      timestamp: timestamp ?? this.timestamp,
+      isDeleted: isDeleted ?? this.isDeleted,
+      deletedBy: deletedBy ?? this.deletedBy,
+      deletedAt: deletedAt ?? this.deletedAt,
+    );
+  }
 }
