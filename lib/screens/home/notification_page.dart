b7de84befdc30eb6a5ad25b05ad7a919dc94359a
Pavithripabasara <pavithripabasara536@gmail.com>
2025-03-17 22:31:25 +0530

    design the code

--- lib/screens/home/notification_page.dart
+++ lib/screens/home/notification_page.dart
@@ -1,151 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:eyedra_ui_v2/widgets/notification_tile.dart';
-import 'package:eyedra_ui_v2/utils/notification_util.dart';
-import 'package:eyedra_ui_v2/models/user_notifications.dart';
+import 'dart:ui';
+import 'package:eyedra_ui_v2/screens/EyedraBot/chatbot.dart';
 
-class NotificationPage extends StatefulWidget {
-  const NotificationPage({Key? key}) : super(key: key);
-
-  @override
-  State<NotificationPage> createState() => _NotificationPageState();
-}
-
-class _NotificationPageState extends State<NotificationPage>
-    with AutomaticKeepAliveClientMixin {
-  late List<UserNotification> _notifications;
-  late List<UserNotification> _filteredNotifications;
-  String _selectedFilter = 'All';
-
-  @override
-  void initState() {
-    super.initState();
-    _notifications = UserNotification.dummyNotifications;
-    _filteredNotifications = _notifications;
-  }
-
-  void _applyFilter() {
-    setState(() {
-      _filteredNotifications = NotificationUtils.filterNotifications(
-          _notifications, _selectedFilter);
-    });
-  }
-
-  void _onFilterChanged(String? newFilter) {
-    if (newFilter != null) {
-      setState(() {
-        _selectedFilter = newFilter;
-        _applyFilter();
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFF240046), // Deep Purple
-                Color(0xFF3C096C), // Darker Purple
-                Color(0xFF5A189A), // Bright Purple
-                Color(0xFF9D4EDD), // Light Purple
-              ],
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-            child: SafeArea(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Hello, User!',
-                    style: textTheme.titleLarge?.copyWith(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
-                      shadows: [
-                        Shadow(
-                          color: Colors.blueAccent.withOpacity(0.6),
-                          blurRadius: 10,
-                        ),
-                      ],
-                    ),
-                  ),
-                  DropdownButton<String>(
-                    value: _selectedFilter,
-                    icon: const Icon(Icons.filter_alt_outlined, color: Colors.white),
-                    items: const [
-                      DropdownMenuItem(value: 'All', child: Text('All')),
-                      DropdownMenuItem(value: 'Today', child: Text('Today')),
-                      DropdownMenuItem(value: 'Yesterday', child: Text('Yesterday')),
-                      DropdownMenuItem(value: 'Last 7 Days', child: Text('Last 7 Days')),
-                    ],
-                    onChanged: _onFilterChanged,
-                    dropdownColor: Colors.white.withOpacity(0.9),
-                    style: const TextStyle(color: Colors.black),
+class CustomAppBar {
+  static PreferredSizeWidget buildGlassmorphicAppBar(BuildContext context,
+      GlobalKey<ScaffoldState> scaffoldKey,
+      String title,
+      VoidCallback onProfileTap) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(100),
+      child: Container(
+        margin: const EdgeInsets.only(top: 40, left: 16, right: 16),
+        height: 60,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: Colors.white.withOpacity(0.1),
+                  width: 0.5,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    blurRadius: 20,
+                    offset: const Offset(0, 5),
                   ),
                 ],
               ),
-            ),
-          ),
-        ),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFF240046), // Deep Purple
-              Color(0xFF3C096C), // Darker Purple
-              Color(0xFF5A189A), // Bright Purple
-              Color(0xFF9D4EDD), // Light Purple
-            ],
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(16.0, 100.0, 16.0, 16.0),
-          child: ListView.builder(
-            itemCount: _filteredNotifications.length,
-            itemBuilder: (_, index) {
-              return Container(
-                margin: const EdgeInsets.only(bottom: 16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.white.withOpacity(0.15), // Glass effect
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      blurRadius: 10,
-                      spreadRadius: 2,
-                      offset: const Offset(0, 4),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.menu, color: Colors.white),
+                    onPressed: () {
+                      scaffoldKey.currentState?.openDrawer();
+                    },
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: _buildColorfulText(title),
                     ),
-                  ],
-                  border: Border.all(color: Colors.white.withOpacity(0.2)),
-                ),
-                child: NotificationTile(
-                  notification: _filteredNotifications[index],
-                ),
-              );
-            },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.person, color: Colors.white),
+                    onPressed: onProfileTap,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) => const ChatbotScreen(),
+                        );
+                      },
+                      child: SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: Image.asset(
+                          'assets/images/eyedra8.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),// Will add row content later
+            ),
           ),
         ),
       ),
     );
   }
+  static Widget _buildColorfulText(String text) {
+    final colors = [
+      const Color(0xFF74A3FF),
+      const Color(0xFF73C5FE),
+      const Color(0xFF73B7FE),
+      const Color(0xFF74A3FF),
+      const Color(0xFF7396FE),
+      const Color(0xFF7490FE),
+    ];
 
-  @override
-  bool get wantKeepAlive => true;
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        for (var i = 0; i < text.length; i++)
+          Text(
+            text[i],
+            style: TextStyle(
+              color: i < colors.length ? colors[i] : colors[i % colors.length],
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+            ),
+          ),
+      ],
+    );
+  }
 }
-
