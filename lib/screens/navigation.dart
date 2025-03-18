eacddb75abc6eadafd56d3df29cd9f0abd08ca2f
Chathnindu <chathnindu@gmail.com>
2025-03-18 15:59:17 +0530

    Made changes to navigation bar

--- lib/screens/navigation.dart
+++ lib/screens/navigation.dart
@@ -1,53 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rive/rive.dart';
 import 'dart:ui';
 
-class CustomBottomNav extends StatelessWidget {
-  final int currentIndex;
-  final Function(int) onTap;
+class CustomGlassmorphicNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
 
-  const CustomBottomNav({
-    super.key,
-    required this.currentIndex,
-    required this.onTap,
-  });
+  const CustomGlassmorphicNavBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(25),
-        child: BackdropFilter(
-          filter:
-              ImageFilter.blur(sigmaX: 15, sigmaY: 15), // Updated blur effect
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.4), // Darker background
-              borderRadius: BorderRadius.circular(25),
-              border: Border.all(
-                color: Colors.white.withOpacity(0.15), // Subtle border
-                width: 1.5,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.blue.withOpacity(0.15),
-                  blurRadius: 25,
-                  spreadRadius: 3,
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(25),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.4),
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(
+                  color: Colors.white.withOpacity(0.15),
+                  width: 1.5,
                 ),
-              ],
-            ),
-            child: BottomNavigationBar(
-              currentIndex: currentIndex,
-              onTap: onTap,
-              items: _buildNavItems(),
-              backgroundColor: Colors.transparent,
-              selectedItemColor: Colors.blue[300], // Darker selected item color
-              unselectedItemColor:
-                  Colors.white.withOpacity(0.9), // Darker unselected item color
-              type: BottomNavigationBarType.fixed,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              elevation: 0,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.15),
+                    blurRadius: 25,
+                    spreadRadius: 3,
+                  ),
+                ],
+              ),
+              child: BottomNavigationBar(
+                currentIndex: selectedIndex,
+                onTap: onItemTapped,
+                type: BottomNavigationBarType.fixed,
+                selectedItemColor: Colors.blue[300],
+                unselectedItemColor: Colors.white.withOpacity(0.9),
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                items: _buildNavItems(),
+              ),
             ),
           ),
         ),
@@ -57,33 +60,52 @@ class CustomBottomNav extends StatelessWidget {
 
   List<BottomNavigationBarItem> _buildNavItems() {
     return [
-      _buildNavItem(Icons.home_outlined, Icons.home, "Home"),
-      _buildNavItem(Icons.people_outline, Icons.people, "People"),
       _buildNavItem(
-          Icons.video_camera_back_outlined, Icons.video_camera_back, "Videos"),
-      _buildNavItem(Icons.perm_media_outlined, Icons.perm_media, "Media"),
+        0,
+        'assets/icons/home-simple-door.svg',
+        'Homepage',
+      ),
       _buildNavItem(
-          Icons.notifications_outlined, Icons.notifications, "Notifications"),
+        1,
+        'assets/icons/group.svg',
+        'Community',
+      ),
+      _buildSpecialNavItem(
+        2,
+        'assets/icons/fire-wood.svg',
+        'assets/icons/campfire.riv',
+        'Campfire Sessions',
+      ),
+      _buildNavItem(
+        3,
+        'assets/icons/posts-carousel-vertical.svg',
+        'Reels',
+      ),
+      _buildNavItem(
+        4,
+        'assets/icons/profile-default.svg',
+        'Profile',
+      ),
     ];
   }
 
   BottomNavigationBarItem _buildNavItem(
-      IconData outlinedIcon, IconData filledIcon, String label) {
+      int index, String iconPath, String label) {
+    bool isSelected = selectedIndex == index;
+
     return BottomNavigationBarItem(
       icon: Container(
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
-          color: currentIndex == _getIndexFromLabel(label)
-              ? Colors.blue.withOpacity(0.3)
-              : Colors.transparent,
+          color: isSelected ? Colors.blue.withOpacity(0.3) : Colors.transparent,
           borderRadius: BorderRadius.circular(12),
-          border: currentIndex == _getIndexFromLabel(label)
+          border: isSelected
               ? Border.all(
                   color: Colors.white.withOpacity(0.15),
                   width: 1,
                 )
               : null,
-          boxShadow: currentIndex == _getIndexFromLabel(label)
+          boxShadow: isSelected
               ? [
                   BoxShadow(
                     color: Colors.blue.withOpacity(0.3),
@@ -93,30 +115,58 @@ class CustomBottomNav extends StatelessWidget {
                 ]
               : null,
         ),
-        child: Icon(
-          currentIndex == _getIndexFromLabel(label) ? filledIcon : outlinedIcon,
-          size: 24,
-          color: Colors.white.withOpacity(0.9), // Darker icon color
+        child: SvgPicture.asset(
+          iconPath,
+          color: isSelected ? Colors.blue[300] : Colors.white.withOpacity(0.9),
+          height: 24,
         ),
       ),
       label: label,
     );
   }
 
-  int _getIndexFromLabel(String label) {
-    switch (label) {
-      case "Home":
-        return 0;
-      case "People":
-        return 1;
-      case "Videos":
-        return 2;
-      case "Media":
-        return 3;
-      case "Notifications":
-        return 4;
-      default:
-        return 0;
-    }
+  BottomNavigationBarItem _buildSpecialNavItem(
+      int index, String iconPath, String rivePath, String label) {
+    bool isSelected = selectedIndex == index;
+
+    return BottomNavigationBarItem(
+      icon: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.blue.withOpacity(0.3) : Colors.transparent,
+          borderRadius: BorderRadius.circular(12),
+          border: isSelected
+              ? Border.all(
+                  color: Colors.white.withOpacity(0.15),
+                  width: 1,
+                )
+              : null,
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.3),
+                    blurRadius: 15,
+                    spreadRadius: 2,
+                  ),
+                ]
+              : null,
+        ),
+        child: isSelected
+            ? SizedBox(
+                height: 28,
+                width: 32,
+                child: RiveAnimation.asset(
+                  rivePath,
+                  fit: BoxFit.contain,
+                ),
+              )
+            : SvgPicture.asset(
+                iconPath,
+                color: Colors.white.withOpacity(0.9),
+                height: 24,
+              ),
+      ),
+      label: label,
+    );
   }
 }
